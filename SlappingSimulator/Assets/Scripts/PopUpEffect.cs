14a955689352464bef982a3d7764e673866b373d
Mattia Traverso <mattiaballa@gmail.com>
2014-07-19 23:59:24 +0200

    Audio and stuff

--- SlappingSimulator/Assets/Scripts/PopUpEffect.cs
+++ SlappingSimulator/Assets/Scripts/PopUpEffect.cs
@@ -6,6 +6,11 @@ public class PopUpEffect : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		ScaleUp ();
+	}
+
+	void ScaleUp()
+	{
 		iTween.ValueTo (gameObject, iTween.Hash ("from", 0f, "to", 0.45f, "onupdate", "changeScale", "ease", iTween.EaseType.easeInOutSine, "time", 0.4f, "oncomplete", "idle"));
 	}
 	
@@ -14,10 +19,31 @@ public class PopUpEffect : MonoBehaviour {
 		this.transform.localScale = Vector3.one * scale;
 	}
 
+	void Update() {
+		if (audio.clip)
+		{
+			if (!audio.isPlaying)
+			{
+				audio.clip = null;
+				iTween.Stop (gameObject);
+				ScaleDown ();
+			}
+		}
+	}
+
 	void idle() {
 		audio.clip = insults[Random.Range (0, insults.Length)];
 		audio.Play ();
 
 		iTween.RotateBy (gameObject, iTween.Hash("x", Random.Range(-0.3f, 0.3f), "easetype", iTween.EaseType.easeInOutBack, "looptype", iTween.LoopType.pingPong, "time", 1.3f));
+	}
+
+	void ScaleDown() {
+		iTween.ValueTo (gameObject, iTween.Hash ("from", 0.45f, "to", 0, "onupdate", "changeScale", "ease", iTween.EaseType.easeInOutSine, "time", 0.15f, "oncomplete", "wait"));
+	}
+
+
+	void wait() {
+
 	}
 }

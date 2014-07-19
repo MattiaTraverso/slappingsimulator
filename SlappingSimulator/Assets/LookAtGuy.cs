f5292cd8e2701e87b02ea34e98996292324ba3e5
Mattia Traverso <mattiaballa@gmail.com>
2014-07-19 19:30:29 +0200

    What about solid colours?

---
+++ SlappingSimulator/Assets/LookAtGuy.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtGuy : MonoBehaviour {
+	public Material[] colors;
+	public GameObject myChild;
+
+	void Start () {
+		myChild.renderer.material = colors[Random.Range (0, colors.Length)];
+	}
+}

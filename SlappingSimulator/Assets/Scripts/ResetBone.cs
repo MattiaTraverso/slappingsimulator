00f7dd248219c21b552c3f9f17d80ae2d665fbe3
Mattia Traverso <mattiaballa@gmail.com>
2014-07-20 06:52:41 +0200

    PRE-BROKEN (ALTHOUGH STILL BROKEN)

---
+++ SlappingSimulator/Assets/Scripts/ResetBone.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetBone : MonoBehaviour {
+	Quaternion oldRotation;
+
+	// Use this for initialization
+	void Start () {
+		oldRotation = transform.rotation;
+	}
+	
+	public void Reset() {
+		transform.rotation = oldRotation;
+	}
+}
